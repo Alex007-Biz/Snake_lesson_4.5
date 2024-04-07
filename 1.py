7f7b286c0836bd578aa474e1f321d35af1bf3990
Alex007-Biz <bizukin@yandex.ru>
2024-04-07 15:30:18 +0300

    3

--- 1.py
+++ 1.py
@@ -1,10 +1,17 @@
 import pygame
 pygame.init()
+
+import time
+
 window_size = (1250, 900)
 screen = pygame.display.set_mode(window_size)
 pygame.display.set_caption("Олеся, я тебя люблю!!!")
-image = pygame.image.load("pic.png")
-image_rect = image.get_rect()
+image1 = pygame.image.load("pic.png")
+image_rect1 = image1.get_rect()
+
+image2 = pygame.image.load("pic2.png")
+image_rect2 = image2.get_rect()
+
 
 speed = 1
 
@@ -17,23 +24,29 @@ while run:
             run = False
         if event.type == pygame.MOUSEMOTION:
             mouseX, mouseY = pygame.mouse.get_pos()
-            image_rect.x = mouseX
-            image_rect.y = mouseY
+            image_rect1.x = mouseX - 130
+            image_rect1.y = mouseY - 110
+
+    if image_rect1.colliderect(image_rect2):
+        print("Произошло!!! Столкновение!!!")
+        time.sleep(1)
 
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        image_rect.x -= speed
+        image_rect2.x -= speed
     if keys[pygame.K_RIGHT]:
-        image_rect.x += speed
+        image_rect2.x += speed
     if keys[pygame.K_UP]:
-        image_rect.y -= speed
+        image_rect2.y -= speed
     if keys[pygame.K_DOWN]:
-        image_rect.y += speed
+        image_rect2.y += speed
 
 
     screen.fill((70, 200, 50))
-    screen.blit(image, image_rect)
+    screen.blit(image1, image_rect1)
+    screen.blit(image2, image_rect2)
+
     pygame.display.flip()
 
 

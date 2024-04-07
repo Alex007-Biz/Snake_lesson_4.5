eabc2857de00c5299898c6090f2222dc24b92936
Alex007-Biz <bizukin@yandex.ru>
2024-04-07 14:18:11 +0300

    2

--- 1.py
+++ 1.py
@@ -2,4 +2,20 @@ import pygame
 pygame.init()
 window_size = (800, 600)
 screen = pygame.display.set_mode(window_size)
-pygame.display.set_caption("тестовый пр")
+pygame.display.set_caption("тестовый проект1")
+image = pygame.image.load("")
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            pygame.quit()
+
+    screen.fill((255, 34, 250))
+    pygame.display.flip()
+
+
+
+pygame.quit()
